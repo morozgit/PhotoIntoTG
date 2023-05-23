a8b1b7714c895a76c8609bd80d8ffe0527e85d69
morozgit <morozec76@gmail.com>
2023-05-23 18:02:52 +0300

    add download_APOD

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def fetch_spacex_last_launch(url, images_path):
     response = requests.get(url)
     response.raise_for_status()
     pictures = response.json()['links']['flickr']['original']
+    print(pictures)
     for picture_number, picture in enumerate(pictures):
         response = requests.get(picture)
         filename = 'Spacex_{0}{1}'.format(picture_number, cut_to_extension(picture))       
@@ -29,14 +30,23 @@ def cut_to_extension(url):
     return extension[1]
 
 
+def download_APOD(url, images_path):
+    payload = {"api_key": "cZHYAr5rNUxpMhgz3FcbL2xeVshvbVAE51wTIgMz"}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    print(response.json()['hdurl'])
+
+
 def main():
     images_path = os.path.join(os.getcwd(), 'images')
     os.makedirs(images_path, exist_ok=True)
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    download_file(url, images_path)
+    # download_file(url, images_path)
     url_spacex = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
-    fetch_spacex_last_launch(url_spacex, images_path)
+    # fetch_spacex_last_launch(url_spacex, images_path)
 
+    url_APOD = 'https://api.nasa.gov/planetary/apod'
+    download_APOD(url_APOD, images_path)
 
 if __name__ == '__main__':
     main()
