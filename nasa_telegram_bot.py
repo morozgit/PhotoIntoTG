ad32bfe156bc5aa09caa4318f0c8961ff94c1dea
morozgit <morozec76@gmail.com>
2023-05-27 14:55:33 +0300

    publish photo to Telegram channel

--- nasa_telegram_bot.py
+++ nasa_telegram_bot.py
@@ -7,5 +7,6 @@ load_dotenv(find_dotenv())
 tg_token = os.environ.get("TG_TOKEN")
 telegram_NASA_bot = telegram.Bot(token=tg_token)
 # chat_id = telegram_NASA_bot.get_updates()[-1].message.chat_id
-telegram_NASA_bot.send_message(chat_id='@photo_nasa_channel', text="I'm sorry Dave I'm afraid I can't do that.")
+# telegram_NASA_bot.send_message(chat_id='@photo_nasa_channel', text="I'm sorry Dave I'm afraid I can't do that.")
+telegram_NASA_bot.send_document(chat_id='@photo_nasa_channel', document=open('images/Spacex_0.jpg', 'rb'))
 print(telegram_NASA_bot)
